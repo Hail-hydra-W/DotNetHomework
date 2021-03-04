f412d15f98906955de26769bf35662604d6c90a0
Hail-hydra-W <why731835846@163.com>
2021-03-04 23:05:46 +0800

    homework1

---
+++ homework1/calculator/calculator/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_number1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_number2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button_calculate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label_time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_number1
+            // 
+            this.textBox_number1.Location = new System.Drawing.Point(133, 149);
+            this.textBox_number1.Name = "textBox_number1";
+            this.textBox_number1.Size = new System.Drawing.Size(125, 31);
+            this.textBox_number1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "数字1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "数字2";
+            // 
+            // textBox_number2
+            // 
+            this.textBox_number2.Location = new System.Drawing.Point(407, 149);
+            this.textBox_number2.Name = "textBox_number2";
+            this.textBox_number2.Size = new System.Drawing.Size(125, 31);
+            this.textBox_number2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "运算符";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(133, 205);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(125, 32);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // button_calculate
+            // 
+            this.button_calculate.Location = new System.Drawing.Point(255, 283);
+            this.button_calculate.Name = "button_calculate";
+            this.button_calculate.Size = new System.Drawing.Size(94, 29);
+            this.button_calculate.TabIndex = 6;
+            this.button_calculate.Text = "计算";
+            this.button_calculate.UseVisualStyleBackColor = true;
+            this.button_calculate.Click += new System.EventHandler(this.button_calculate_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(319, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "运算结果";
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Location = new System.Drawing.Point(407, 205);
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.Size = new System.Drawing.Size(125, 31);
+            this.textBox_result.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("OPPOSans M", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(227, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 36);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "简易计算器";
+            // 
+            // label_time
+            // 
+            this.label_time.AutoSize = true;
+            this.label_time.Location = new System.Drawing.Point(326, 84);
+            this.label_time.Name = "label_time";
+            this.label_time.Size = new System.Drawing.Size(54, 24);
+            this.label_time.TabIndex = 10;
+            this.label_time.Text = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 386);
+            this.Controls.Add(this.label_time);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.button_calculate);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_number2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_number1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_number1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_number2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button_calculate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label_time;
+    }
+}
+
---
+++ homework1/calculator/calculator/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("+");
+            comboBox1.Items.Add("-");
+            comboBox1.Items.Add("×");
+            comboBox1.Items.Add("÷");
+            label_time.Text = DateTime.Now.ToString();  //设置label_time的文本为当前的系统时间
+        }
+
+        private void button_calculate_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            double c = 0;
+            double number1 = 0;
+            double number2 = 0;
+            //异常处理
+            try
+            {
+                number1 = Double.Parse(textBox_number1.Text);
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show("第一个加数异常", e1.Message);
+                return;
+            }
+            try
+            {
+                number2 = Double.Parse(textBox_number2.Text);
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show("第二个加数异常", e2.Message);
+                return;
+            }
+            a = Convert.ToDouble(textBox_number1.Text);
+            b = Convert.ToDouble(textBox_number2.Text);
+
+            switch (comboBox1.Text)
+            {
+                case "+":
+                    c = a + b;
+                    break;
+                case "-":
+                    c = a - b;
+                    break;
+                case "×":
+                    c = a * b;
+                    break;
+                case "÷":
+                    //异常处理
+                    if (b == 0)
+                    {
+                        MessageBox.Show("除数不能为0", "错误", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        c = a / b;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            textBox_result.Text = c.ToString();
+        }
+
+    }
+}
