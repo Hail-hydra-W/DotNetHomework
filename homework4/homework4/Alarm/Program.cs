4e8627ff3f2bb34ad4f22c3e4d9a889fbab75541
Hail-hydra-W <why731835846@163.com>
2021-03-25 23:27:57 +0800

    homework4

---
+++ homework4/homework4/Alarm/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Threading;
+
+namespace Alarm
+{
+    public delegate void TickHandler(object sender, Alarm.TimeArgs tickArgs);
+
+    public delegate void AlarmHandler(object sender, Alarm.TimeArgs eventArgs);
+
+
+    public class Alarm
+    {
+        public event TickHandler Tick;
+        public event AlarmHandler Clock;
+
+
+        private void OnTick(TimeArgs tickArgs)
+        {
+            Tick?.Invoke(this, tickArgs);
+        }
+
+        private void OnClock(TimeArgs eventArgs)
+        {
+            Clock?.Invoke(this, eventArgs);
+        }
+
+        //每个一秒触发一次Tick事件
+        //每个一分钟触发一次Clock事件
+        public void Start(TimeArgs timeArgs)
+        {
+            //获取当前时间
+            DateTime now = DateTime.Now;
+            var eventArgs = new TimeArgs(now.Hour, now.Minute, now.Second);
+
+            while (true)
+            {
+                eventArgs.Seconds++;
+                OnTick(eventArgs);
+                Thread.Sleep(1000);
+                if (!eventArgs.Equals(timeArgs))
+                {
+                    continue;
+                }
+                OnClock(eventArgs);
+            }
+
+        }
+
+        public class TimeArgs : EventArgs
+        {
+            private int hours;
+
+
+            private int minutes;
+            private int seconds;
+
+            public TimeArgs(int hour, int minute, int second)
+            {
+                hours = hour;
+                minutes = minute;
+                seconds = second;
+            }
+
+            public TimeArgs()
+            {
+            }
+
+            public int Hours
+            {
+                get => hours;
+                set
+                {
+                    hours = value;
+                    if (hours == 24)
+                    {
+                        hours = 0;
+                    }
+                }
+            }
+
+
+            public int Minutes
+            {
+                get => minutes;
+                set
+                {
+                    minutes = value;
+                    if (minutes != 60) return;
+                    minutes = 0;
+                    Hours++;
+                }
+            }
+
+
+            public int Seconds
+            {
+                get => seconds;
+                set
+                {
+                    seconds = value;
+                    if (seconds != 60) return;
+                    seconds = 0;
+                    Minutes++;
+                }
+            }
+
+            public override bool Equals(object obj)
+            {
+                TimeArgs timeArgs = obj as TimeArgs;
+                if (timeArgs == null)
+                {
+                    return false;
+                }
+
+                return Hours == timeArgs.Hours &&
+                       Minutes == timeArgs.Minutes &&
+                       Seconds == timeArgs.Seconds;
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    var hashCode = Hours;
+                    hashCode = (hashCode * 397) ^ Minutes;
+                    hashCode = (hashCode * 397) ^ Seconds;
+                    return hashCode;
+                }
+            }
+        }
+    }
+
+
+    class Program
+    {
+            public static void Main(string[] args)
+            {
+                Alarm alarm = new Alarm();
+                alarm.Tick += ShowTime;
+                alarm.Clock += Clock;
+                alarm.Start(new Alarm.TimeArgs(4, 44, 0));
+            }
+
+            private static void ShowTime(object sender, Alarm.TimeArgs eventArgs)
+            {
+                Console.WriteLine("过了一秒，现在的时间是" +
+                                  $"{eventArgs.Hours}点时{eventArgs.Minutes}分{eventArgs.Seconds}秒");
+            }
+
+            private static void Clock(object sender, Alarm.TimeArgs eventArgs)
+            {
+                Console.WriteLine("闹钟到点");
+            }
+    }
+}
---
+++ homework4/homework4/lambda/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace lambda
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T data)
+        {
+            Next = null;
+            Data = data;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new GenericList<int>();
+            var total = 0;
+
+
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(random.Next(20));
+            }
+
+            int min;
+            int max = min = list.Head.Data;
+            list.ForEach(Console.WriteLine);
+            list.ForEach(d => total += d);
+            Console.WriteLine($"和为{total}");
+            list.ForEach(d =>
+            {
+                if (min > d)
+                {
+                    min = d;
+                }
+            });
+            Console.WriteLine($"最小值为{min}");
+            list.ForEach(d =>
+            {
+                if (max < d)
+                {
+                    max = d;
+                }
+            });
+            Console.WriteLine($"最大值为{max}");
+        }
+    }
+}
