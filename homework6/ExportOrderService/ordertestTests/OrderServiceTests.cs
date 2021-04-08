ca00e57f10d2f735625d173c0e105a974481f7a5
Hail-hydra-W <why731835846@163.com>
2021-04-09 00:00:13 +0800

    homework6

---
+++ homework6/ExportOrderService/ordertestTests/OrderServiceTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ordertest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordertest.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+       
+        [TestMethod()]
+        public void AddOrderTest()
+        {
+                Customer customer1 = new Customer(1, "Customer1");
+                Customer customer2 = new Customer(2, "Customer2");
+
+                Goods milk = new Goods(1, "Milk", 69.9f);
+                Goods eggs = new Goods(2, "eggs", 4.99f);
+                Goods apple = new Goods(3, "apple", 5.59f);
+
+                Order order1 = new Order(1, customer1);
+                order1.AddDetails(new OrderDetail(apple, 8));
+                order1.AddDetails(new OrderDetail(eggs, 10));
+                Order order2 = new Order(2, customer2);
+                order2.AddDetails(new OrderDetail(eggs, 10));
+                order2.AddDetails(new OrderDetail(milk, 10));
+
+                Order order3 = new Order(3, customer2);
+                order3.AddDetails(new OrderDetail(milk, 100));
+
+                OrderService orderService = new OrderService();
+                orderService.AddOrder(order1);
+                orderService.AddOrder(order2);
+                orderService.AddOrder(order3);
+            List<Order> target=new List<Order>();
+            target.Add(order1);target.Add(order2);target.Add(order3);
+            Assert.AreEqual(target,orderService.orderList);
+        }
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            Customer customer1 = new Customer(1, "Customer1");
+            Customer customer2 = new Customer(2, "Customer2");
+
+            Goods milk = new Goods(1, "Milk", 69.9f);
+            Goods eggs = new Goods(2, "eggs", 4.99f);
+            Goods apple = new Goods(3, "apple", 5.59f);
+
+            Order order1 = new Order(1, customer1);
+            order1.AddDetails(new OrderDetail(apple, 8));
+            order1.AddDetails(new OrderDetail(eggs, 10));
+            Order order2 = new Order(2, customer2);
+            order2.AddDetails(new OrderDetail(eggs, 10));
+            order2.AddDetails(new OrderDetail(milk, 10));
+
+            Order order3 = new Order(3, customer2);
+            order3.AddDetails(new OrderDetail(milk, 100));
+
+            OrderService orderService = new OrderService();
+            orderService.AddOrder(order1);
+            orderService.AddOrder(order2);
+            orderService.AddOrder(order3);
+            Order Target = order3;
+            Order ById = orderService.GetById(3);
+            Assert.AreEqual(Target,ById);
+        }
+
+    }
+}
